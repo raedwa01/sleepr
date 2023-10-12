b552a5224672738c7931227bcc8953be64433402
Rob Edwards <REdwards@techstyle.com>
2023-10-12 19:55:05 -0400

    Additional Health fix

--- libs/common/src/health/health.module.ts
+++ libs/common/src/health/health.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { HealthController } from './health.controller';
 
-@Module()
+@Module({
+  controllers: [HealthController],
+})
 export class HealthModule {}
